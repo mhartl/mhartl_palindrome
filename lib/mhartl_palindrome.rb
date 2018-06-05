1c8b212a4261d983b898a3fbe9e25dbaca21f239
Michael Hartl <michael@michaelhartl.com>
2018-06-04 20:45:16 -0700

    Polish

--- lib/mhartl_palindrome.rb
+++ lib/mhartl_palindrome.rb
@@ -12,19 +12,3 @@ class String
     processed_content == processed_content.reverse
   end
 end
-
-
-# module MhartlPalindrome
-#   class Phrase < String
-
-#     # Processes content for palindrome testing.
-#     def processed_content
-#       self.downcase
-#     end
-
-#     # Returns true for a palindrome, false otherwise.
-#     def palindrome?
-#       processed_content == processed_content.reverse
-#     end
-#   end
-# end
